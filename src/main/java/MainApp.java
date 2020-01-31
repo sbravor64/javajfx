6e89ca9b3246ea00f0523bda3fbfd29b237d9a4e
sbravor64 <Andresbravo97>
2020-01-31 19:49:44 +0100

    Initial commit

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setTitle("hola payaso");
+        Scene scene = new Scene(root, 600,400);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/control/SampleController.java
@@ -0,0 +1,14 @@
+package control;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import java.awt.*;
+
+public class SampleController {
+
+    @FXML
+    public void onClick(ActionEvent event) {
+        System.out.println("hola");
+    }
+
+}
